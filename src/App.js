358d322d3deca643ad7848d79f3511aebffae373
AleksandarBrko <a.bajceta33@gmail.com>
2021-08-18 16:29:27 +0200

    component App created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    // use super so we can access "this"
+    super(props);
+    // here we have initial state
+    this.state = { loggedon: true };
+  }
+
+  render() {
+    return <div>Hello</div>;
+  }
 }
 
 export default App;
