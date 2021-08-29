27829c7af9c128659fae62687ad2a126f088aaa5
aleksandar <a.bajceta33@gmail.com>
2021-08-29 11:22:32 +0200

    added css 1.0.1

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,10 +17,12 @@ class Main extends Component {
         <div className="main-section">
           <div className="egypt-section">
             <div className="textE">
-              The history of Egypt has been long and wealthy, due to the flow of
-              the Nile River with its fertile banks and delta, as well as the
-              accomplishments of Egypt's native inhabitants and outside
-              influence
+              <a href="https://en.wikipedia.org/wiki/History_of_Egypt">
+                The history of Egypt has been long and wealthy, due to the flow
+                of the Nile River with its fertile banks and delta, as well as
+                the accomplishments of Egypt's native inhabitants and outside
+                influence...
+              </a>
             </div>
           </div>
         </div>
