9261f8955d46c3d5cbdb394825c02994d9b222af
aleksandar <a.bajceta33@gmail.com>
2021-08-31 17:25:53 +0200

    finishing main css 1.0

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -9,10 +9,51 @@ class ContactForm extends Component {
   }
 
   render() {
-    return <div className="contactform-container">
-      <div className="contactForm-title"><h2>CONTACT FORM</h2></div>
-      <div className="contactrForm-form"></div>
-    </div>;
+    return (
+      <div className="contactform-container">
+        <div className="contactForm-title">
+          <h2>CONTACT FORM</h2>
+        </div>
+        <div className="section">
+          <div className="contForm">
+            <form name="contact" method="POST" action="sucess">
+              <div className="nameInput">
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="emailInput">
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email" />
+              </div>
+              <div className="fieldInput">
+                <label for="message">Message</label>
+                <textarea name="message" id="message" rows="6" />
+              </div>
+              <ul className="sendMessage">
+                <li>
+                  <input type="submit" value="Send message" />
+                </li>
+              </ul>
+            </form>
+          </div>
+          <div className="contactForm-info">
+            <div className="info email">
+              <h3>Email</h3>
+              <a href="mailto:a.bajceta33@gmail.com">a.bajceta33@gmail.com</a>
+            </div>
+            <div className="info phone">
+              <h3>Phone</h3>
+              <p>064/051 0451</p>
+            </div>
+            <div className="info location">
+              <h3>Locations</h3>
+              <p>Belgrade</p>
+              <p>Vienna</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
